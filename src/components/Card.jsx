50ff6bda570a366c0f66dc2a6c1dee278ad5c024
Josehugo <josehugo.tec@gmail.com>
2026-07-05 22:48:10 -0500

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,3 +0,0 @@
-export default function Card({ title, icon, children, action, className='' }) {
-  return <section className={`card ${className}`}><div className="cardHead"><h3>{icon} {title}</h3>{action}</div>{children}</section>;
-}
